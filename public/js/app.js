557ec44d73ebcd8863c1bca1b94e54a93b856601
kanikas01 <larvakurt@yahoo.com>
2019-04-19 10:12:17 -0700

    Move all functions into app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
 
   $(document).on("click", ".btn.scrape-new", scrapeArticles);
   $(document).on("click", ".btn.clear", deleteUnsavedArticles);
+  $(document).on("click", ".btn.save", saveArticles);
+  $(document).on("click", ".btn.delete", handleArticleDelete);
 
   // ---------- Event Handlers ---------- //
 
@@ -25,6 +27,35 @@ $(document).ready(function() {
       url: "/articles"
     }).then(function() {
       location.reload();
+    });
+  }
+
+  // Save articles
+  function saveArticles() {
+    let thisCard = $(this).parents(".card");
+    let thisId = $(this)
+      .parents(".card")
+      .attr("data-id");
+
+    $.ajax({
+      method: "PUT",
+      url: "/save-article/" + thisId
+    }).then(function() {
+      thisCard.remove();
+    });
+  }
+
+  function handleArticleDelete() {
+    let thisCard = $(this).parents(".card");
+    let thisId = $(this)
+      .parents(".card")
+      .attr("data-id");
+
+    $.ajax({
+      method: "PUT",
+      url: "/remove-article/" + thisId
+    }).then(function() {
+      thisCard.remove();
     });
   }
 });
