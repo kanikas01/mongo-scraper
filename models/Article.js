8ca2876cd8bc6eb890a69780dc6fae07f3a21f45
kanikas01 <larvakurt@yahoo.com>
2019-04-20 15:30:38 -0700

    Remove extra comments

--- models/Article.js
+++ models/Article.js
@@ -4,14 +4,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 const ArticleSchema = new Schema({
-  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
   link: {
     type: String,
     required: true,
--- models/Note.js
+++ models/Note.js
@@ -4,11 +4,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new NoteSchema object
-// This is similar to a Sequelize model
 const NoteSchema = new Schema({
-  // `title` is of type String
-  title: String,
-  // `body` is of type String
   body: String
 });
 
